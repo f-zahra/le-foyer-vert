161be1c3e3c975a9bcc6cb26457e542e48531249
f-zahra <eva-200@hotmail.fr>
2025-11-24 08:42:21 -0500

    update favicon for site branding

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -53,6 +53,22 @@ export default async function RootLayout({
 
   return (
     <html lang={locale}>
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className={`${montserrat.variable} antialiased `}>
         <TopBar companyInfo={companyInfo}></TopBar>
         <NextIntlClientProvider messages={messages}>

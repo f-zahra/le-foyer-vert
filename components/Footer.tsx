5e238a660581d0ea9fada906c217f4862352b5b8
f-zahra <eva-200@hotmail.fr>
2025-11-27 11:21:37 -0500

    add border

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import { FaInstagram, FaFacebook } from "react-icons/fa";
 import { CompanyInformation } from "@/lib/companyInformation";
 const Footer = ({ companyInfo }: { companyInfo: CompanyInformation }) => {
   return (
-    <footer className="bg-[#b4c2bb] text-green-950">
+    <footer className="bg-[#b4c2bb] text-green-950 border-t border-green-950/30 ">
       <div className="@container px-10 mx-auto">
         <div className="py-16 xl:py-4 flex flex-col xl:flex-row gap-[60px] xl:gap-[30px] xl:items-center justify-evenly mb-6">
           {/* logo & text  */}
@@ -31,15 +31,15 @@ const Footer = ({ companyInfo }: { companyInfo: CompanyInformation }) => {
             </h4>
             <ul className="space-y-4 text-foreground">
               <li className="flex items-center gap-4">
-                <MapPin size={16} strokeWidth={2} />
+                <MapPin size={16} strokeWidth={2} className="shrink-0" />
                 <p>{companyInfo.address} </p>
               </li>
               <li className="flex items-center gap-4">
-                <Mail size={16} strokeWidth={2} />
+                <Mail size={16} strokeWidth={2} className="shrink-0" />
                 <p>{companyInfo.email}</p>
               </li>
               <li className="flex items-center gap-4">
-                <Phone size={16} strokeWidth={2} />
+                <Phone size={16} strokeWidth={2} className="shrink-0" />
                 <p>{companyInfo.phone}</p>
               </li>
               <li className="flex items-center gap-4 mt-10  text-2xl">

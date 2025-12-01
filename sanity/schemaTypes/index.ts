4f3b35c39393ca0afed2559de29af24269350d9e
f-zahra <eva-200@hotmail.fr>
2025-11-30 19:15:08 -0500

    remove testimonials schema import from schema index

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,7 +2,6 @@ import serviceSchema from './service'
 import aboutSchema from './about'
 import gallerySchema from './photoGallerySchema'
 import regionSchema from './serviceArea'
-import testimonialSchema from './testimonials'
 import {companyInformationSchema} from './companyInformation'
 import heroSchema from './hero'
 import {localeString} from './localeString'
@@ -13,7 +12,6 @@ export const schemaTypes = [
   aboutSchema,
   gallerySchema,
   regionSchema,
-  testimonialSchema,
   companyInformationSchema,
   heroSchema,
   localeString,
